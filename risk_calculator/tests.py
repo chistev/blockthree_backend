2df8d9d5b05e77ebb817eb0b921500fd9fb387a2
chistev <stephenowabie@gmail.com>
2025-08-10 17:27:17 +0100

    tests for simulate_btc_paths

--- risk_calculator/tests.py
+++ risk_calculator/tests.py
@@ -1,8 +1,12 @@
 import unittest
 from unittest.mock import Mock, patch
 import json
-from django.http import HttpResponse
 import requests
+
+import unittest
+import numpy as np
+from risk_calculator.views import simulate_btc_paths, DEFAULT_PARAMS
+
 
 from risk_calculator.views import get_json_data, fetch_btc_price, validate_inputs, DEFAULT_PARAMS
 
@@ -79,3 +83,53 @@ class TestRiskCalculatorFunctions(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             validate_inputs(params)
         self.assertEqual(str(context.exception), "BTC_purchased cannot be negative")
+
+class TestSimulateBTCPaths(unittest.TestCase):
+
+    def setUp(self):
+        self.params = DEFAULT_PARAMS.copy()
+        self.params['paths'] = 10000 # smaller for test speed
+        self.params['t'] = 1
+        np.random.seed(42)
+
+    def test_output_shapes(self):
+        btc_prices, vol_heston = simulate_btc_paths(self.params)
+        self.assertIsInstance(btc_prices, np.ndarray)
+        self.assertIsInstance(vol_heston, np.ndarray)
+        self.assertEqual(len(btc_prices), self.params['paths'])
+        self.assertEqual(len(vol_heston), self.params['paths'] - 1)  # from log_returns length
+
+    def test_prices_are_positive(self):
+        btc_prices, _ = simulate_btc_paths(self.params)
+        self.assertTrue(np.all(btc_prices > 0))
+
+    def test_volatility_positive(self):
+        _, vol_heston = simulate_btc_paths(self.params)
+        self.assertTrue(np.all(vol_heston > 0))
+
+    def test_increasing_sigma_increases_variability(self):
+        np.random.seed(42)
+        prices_low_sigma, _ = simulate_btc_paths({**self.params, 'sigma': 0.1})
+        np.random.seed(42)
+        prices_high_sigma, _ = simulate_btc_paths({**self.params, 'sigma': 1.0})
+        var_low = np.var(np.diff(prices_low_sigma))
+        var_high = np.var(np.diff(prices_high_sigma))
+        self.assertGreater(var_high, var_low)
+
+    def test_jump_intensity_affects_price_jumps(self):
+        np.random.seed(42)
+        prices_no_jump, _ = simulate_btc_paths({**self.params, 'jump_intensity': 0.0})
+        np.random.seed(42)
+        prices_with_jump, _ = simulate_btc_paths({**self.params, 'jump_intensity': 1.0})
+        # With high jump_intensity, variance should be noticeably higher
+        self.assertGreater(np.var(np.diff(prices_with_jump)), np.var(np.diff(prices_no_jump)))
+
+    def test_target_price_adjustment(self):
+        np.random.seed(42)
+        params_target_high = {**self.params, 'targetBTCPrice': self.params['BTC_current_market_price'] * 2}
+        prices_high_target, _ = simulate_btc_paths(params_target_high)
+        np.random.seed(42)
+        params_target_low = {**self.params, 'targetBTCPrice': self.params['BTC_current_market_price'] * 0.5}
+        prices_low_target, _ = simulate_btc_paths(params_target_low)
+        # On average, higher target price should push simulated final price higher
+        self.assertGreater(np.mean(prices_high_target), np.mean(prices_low_target))
